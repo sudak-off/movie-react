8c70b94a07bf3a69a64c07ad1a9be01762d976df
Tema <krestonos@gmail.com>
2022-04-05 18:31:04 +0300

    step compos

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 
 @Controller('/api')
 export class AppController {
 
+    constructor(private appService: AppService) {
+
+    }
     @Get()
     getUsers() {
-        return 'Get all user'
+        return this.appService.getUsers()
     }
 
 }
