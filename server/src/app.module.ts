e32c8e9e18c998236c284b0966527aaaad42a005
Tema <krestonos@gmail.com>
2022-04-07 17:01:47 +0300

    add file music and picture

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { FileModule } from "./file/file.module";
 import { TrackModule } from './track/track.module';
 
 
@@ -7,6 +8,7 @@ import { TrackModule } from './track/track.module';
     imports: [
         MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.xl0fc.mongodb.net/music-react?retryWrites=true&w=majority'),
         TrackModule,
+        FileModule,
     ],
 })
 export class AppModule { }
