9e9ea166b072de6e943ee8e9381421382d452847
Tema <krestonos@gmail.com>
2022-04-16 21:38:04 +0300

    add step 1 form

--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, StepLabel } from '@mui/material'
+import { Button, Grid, StepLabel, TextField } from '@mui/material'
 import React from 'react'
 import StepWrapper from '../../components/StepWrapper'
 import { MainLayout } from '../../layouts/MainLayout'
@@ -7,7 +7,9 @@ const Create = () => {
     const [activeStep, setActiveStep] = React.useState(0)
 
     const next = () => {
-        setActiveStep(prev => prev + 1)
+        if (activeStep !== 2) {
+            setActiveStep(prev => prev + 1)
+        }
     }
 
     const back = () => {
@@ -17,7 +19,30 @@ const Create = () => {
     return (
         <MainLayout>
             <StepWrapper activeStep={activeStep}>
-                <h1>Загрузка трека</h1>
+                {activeStep === 0 &&
+                    <Grid container direction={"column"} style={{ padding: 20 }}>
+                        <TextField
+                            style={{ marginTop: 10 }}
+                            label={"Название трека"}
+                        />
+                        <TextField
+                            style={{ marginTop: 10 }}
+                            label={"Имя исполнителя"}
+                        />
+                        <TextField
+                            style={{ marginTop: 10 }}
+                            label={"Слова к треку"}
+                            multiline
+                            rows={3}
+                        />
+                    </Grid>
+                }
+                {activeStep === 1 &&
+                    <h1>Step 2</h1>
+                }
+                {activeStep === 2 &&
+                    <h1>Step 3</h1>
+                }
             </StepWrapper>
             <Grid container justifyContent="space-between">
                 <Button disabled={activeStep === 0} onClick={back}>Назад</Button>
